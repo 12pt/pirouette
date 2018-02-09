3dfee333562a145e5048f5b82100c26114e2c599
12pt <12pt@users.noreply.github.com>
2018-02-09 09:18:38 +0000

    Refactoring

--- src/UrlParser.php
+++ src/UrlParser.php
@@ -2,45 +2,15 @@
 
 # split a /path/to/$key into its constituent parts and provide comparison routes.
 class UrlParser {
-
-    // /**
-    //  * @param string $targetUrl the url (+ optional placeholders) to match actual urls against
-    //  */
-    // public function __construct(string $targetUrl) {
-    //     # url can contain regex
-    //     # url can contain placeholders e.g. {id}
-    //     $path = explode($targetUrl, "/");
-    //     $constructed = [];
-    //     foreach($path as $element) {
-    //         array_push($constructed, _parseElement($element));
-    //     }
-    // }
+    const OPEN_PLACEHOLDER = "{";
+    const CLOSE_PLACEHOLDER = "}";
 
     /**
      * Convert a single element between /'s into either a placeholder or not.
-     * Only support the entire thing being a placeholder, for now anyway.
-     * it would be easy to add support for "post{id}" in the future but it's questionable
-     * to have parts of the url that look static but aren't.
+     * @param string $urlBit the url "bit" e.g. text between two /'s.
      */
     public function parseElement(string $urlBit) {
-        $isQuoted = false;
-        $quotedStr = "";
-
-        for($i=0; $i<strlen($urlBit); $i++) {
-            $chara = $urlBit[$i];
-            
-            if($isQuoted) {
-                if($chara == "}") {
-                    $isQuoted = false;
-                } else {
-                    $quotedStr .= $chara;
-                }
-            } else {
-                if($chara == "{") {
-                    $isQuoted = true;
-                }
-            }
-        }
+        $quotedStr = UrlParser::isPlaceholder($urlBit);
 
         if(strlen($quotedStr) > 0) {
             return array("type" => "placeholder",
@@ -50,12 +20,36 @@ class UrlParser {
                          "value" => $urlBit);
         }
     }
-
+ 
     /**
-     * Match a given url e.g. /blog/post/34 to a template url /blog/post/{id}
+     * Check if a given string is a URL placeholder. For now, it's just if it is a word surrounded in {}.
+     * We could use regex but doing this here allows for future modification.
+     * Only support the entire thing being a placeholder, for now anyway.
+     *
+     * @param string $urlBit the url "bit" e.g. text between two /'s.
+     * @return the placeholder string, or an empty string if no placeholder was found.
      */
-    public function match(string $url, mixed $kvs) {
-        # return array('id' => 34);
+    public static function isPlaceholder(string $urlBit) {
+        $quotedStr = "";
+        $isQuoted = false;
+
+        for($i=0; $i<strlen($urlBit); $i++) {
+            $chara = $urlBit[$i];
+            
+            if($isQuoted) {
+                if($chara == self::CLOSE_PLACEHOLDER) {
+                    $isQuoted = false;
+                } else {
+                    $quotedStr .= $chara;
+                }
+            } else {
+                if($chara == self::OPEN_PLACEHOLDER) {
+                    $isQuoted = true;
+                }
+            }
+        }
+
+        return strlen($quotedStr) > 0 ? $quotedStr : "";
     }
 }
 ?>
