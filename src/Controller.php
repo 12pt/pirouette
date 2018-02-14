b079a186619e35bffb33bc5a796203bb88636b57
12pt <12pt@users.noreply.github.com>
2018-02-14 00:19:16 +0000

    Further documentation style compliance.

--- src/Controller.php
+++ src/Controller.php
@@ -5,23 +5,27 @@ namespace Pirouette;
 require_once(__DIR__ . "/Path.php");
 
 class Controller {
-    private $paths;
+    private $_paths;
 
     /**
      * Initialise the paths variable where we store ALL the routes, along with
      * the related HTTP verb that executes a callback function also stored here.
+     *
+     * @return void
      */
     public function __construct() {
-        $this->paths = [];
+        $this->_paths = [];
     }
 
     /**
      * Called automatically once the user has declared all their routes, this "kicks off" the
      * routing.
+     *
+     * @return void
      */
     private function _commence() {
-        if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] != null && $_SERVER["REQUEST_URI"] != null) {
-            $this->route($_SERVER["REQUEST_METHOD"], strtok($_SERVER["REQUEST_URI"], "?"));
+        if (isset($_SERVER["REQUEST_METHOD"]) && isset($_SERVER["REQUEST_URI"])) {
+            $this->_route($_SERVER["REQUEST_METHOD"], strtok($_SERVER["REQUEST_URI"], "?"));
         }
     }
 
@@ -34,7 +38,7 @@ class Controller {
      *
      * @return void
      */
-    private function route(string $method, string $path) {
+    private function _route(string $method, string $path) {
         switch($method) {
         case "GET":
         case "POST":
@@ -59,12 +63,12 @@ class Controller {
      * @return void
      */
     private function _addListener(string $path, string $method, callable $callback) {
-        if(!isset($this->paths[$path])) {
-            $this->paths[$path] = [];
+        if (!isset($this->_paths[$path])) {
+            $this->_paths[$path] = [];
         }
-        array_push($this->paths[$path], array("path" => new Path($path), # the generalised path for this fn
-                                              "method" => $method, # the HTTP verb we want to associate this fn with
-                                              "callback" => $callback)); # the fn to call
+        array_push($this->_paths[$path], array("path" => new Path($path),
+                                              "method" => $method,
+                                              "callback" => $callback));
     }
 
     /**
@@ -75,33 +79,32 @@ class Controller {
      *
      * @return $urlKeywords plus a populated ["_params"].
      */
-    private function populateRequest(array $urlKeywords, string $method) {
-        # obtain data from superglobals:
-        if(isset($_GET) && count($_GET) > 0) {
+    private function _populateRequest(array $urlKeywords, string $method) {
+        // obtain data from superglobals:
+        if (isset($_GET) && count($_GET) > 0) {
             $urlKeywords["_params"] = $_GET;
         }
-        if(isset($_POST) && count($_POST) > 0) {
+        if (isset($_POST) && count($_POST) > 0) {
             $urlKeywords["_params"] = $_POST;
         }
-        if(isset($_FILES) && count($_FILES) > 0) {
+        if (isset($_FILES) && count($_FILES) > 0) {
             $urlKeywords["_files"] = $_FILES;
         }
 
-        # obtain data from php://input
-        if($method == "PUT") {
+        // obtain data from php://input
+        if ($method == "PUT") {
             try {
                 parse_str(file_get_contents("php://input"), $put);
-                if(isset($put) && count($put) > 0) {
+                if (isset($put) && count($put) > 0) {
                     $urlKeywords["_params"] = $put;
                 }
             } catch(Exception $e) {
                 error_log("Error trying to read parameters from put request: " . $e->getMessage());
             }
-        }
-        elseif($method == "POST") {
+        } elseif ($method == "POST") {
             try {
                 $data = json_decode(file_get_contents("php://input"), true);
-                if(isset($data) && count($data) > 0) {
+                if (isset($data) && count($data) > 0) {
                     $urlKeywords["_json"] = $data;
                 }
             } catch (Exception $e) {
@@ -123,19 +126,19 @@ class Controller {
      * @return void
      */
     public function router(string $path, string $method) {
-        foreach($this->paths as $p) {
-            foreach($p as $path_listener) {
-                # perform no extra logic if this request is a different HTTP verb
-                if($path_listener["method"] == $method) {
+        foreach ($this->_paths as $p) {
+            foreach ($p as $path_listener) {
+                // perform no extra logic if this request is a different HTTP verb
+                if ($path_listener["method"] == $method) {
                     $matches = $path_listener["path"]->matchPath($path);
 
-                    # if the current path can have a generalised path applied to it:
-                    if(isset($matches)) {
-                        $request = $this->populateRequest($matches, $method);
+                    // if the current path can have a generalised path applied to it:
+                    if (isset($matches)) {
+                        $request = $this->_populateRequest($matches, $method);
                         $result = $path_listener["callback"]($request);
 
-                        # if the user made the anonymous callback return something, echo it to the client.
-                        if(isset($result)) {
+                        // if something is returned, echo it to the client.
+                        if (isset($result)) {
                             echo($result);
                         }
                     }
@@ -168,7 +171,7 @@ class Controller {
     /**
      * Set up a route for POST requests.
      * 
-     * @param string   $path the generalised path e.g. /blog/posts/{id}
+     * @param string   $path     the generalised path e.g. /blog/posts/{id}
      * @param callable $callback what to do on this path, should accept associative array as parameter.
      *
      * @return void
@@ -180,7 +183,7 @@ class Controller {
     /**
      * Set up a route for PUT requests.
      * 
-     * @param string   $path the generalised path e.g. /blog/posts/{id}
+     * @param string   $path     the generalised path e.g. /blog/posts/{id}
      * @param callable $callback what to do on this path, should accept associative array as parameter.
      *
      * @return void
@@ -192,7 +195,7 @@ class Controller {
     /**
      * Set up a route for DELETE requests.
      * 
-     * @param string   $path the generalised path e.g. /blog/posts/{id}
+     * @param string   $path     the generalised path e.g. /blog/posts/{id}
      * @param callable $callback what to do on this path, should accept associative array as parameter.
      *
      * @return void
